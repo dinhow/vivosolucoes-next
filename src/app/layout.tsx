292a2c3d27530a5d67257419bb29ea7b10d4ea09
edmilsons2025 <edmilsons.positivo@gmail.com>
2025-08-29 14:02:44 -0300

    Adicionado Google Tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17517818238"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-17517818238');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => (
   <div className="mt-8 pt-4 border-t border-gray-200 text-center text-xs text-gray-400">
     <p>Representante <span className="font-normal text-gray-500">Evolua Digital</span>, parceira <span className="font-normal text-gray-500">Vivo Empresas</span>.</p>
-    <p>Página Desenvolvida por: <a href="https://www.linkedin.com/in/dinho-cristenson/" target="_blank" className="text-gray-400 hover:underline">Edmilson R. Cristenson da Silva</a></p>
+    {/* <p>Página Desenvolvida por: <a href="https://www.linkedin.com/in/dinho-cristenson/" target="_blank" className="text-gray-400 hover:underline">Edmilson R. Cristenson da Silva</a></p> */}
   </div>
 );
 
